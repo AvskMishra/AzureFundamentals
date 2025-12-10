696f798dbd5b6b109ef61fb31c617cf9143f10e2
Abhishek Kumar <akkr0077@gmail.com>
2025-12-10 14:04:28 +0530

    🚧 Added code for Blob Crud Operation  with MVC
    📝Updated bloc connection string for repo commit
    
    🔥 removed blob conncetion string  due to code-security

---
+++ AzureBlobProject/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using AzureBlobProject.Models;
+using AzureBlobProject.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IContainerService _containerService;
+        private readonly ILogger _logger;
+        public HomeController(ILogger<HomeController> logger,IContainerService containerService)
+        {
+            _logger = logger;
+            _containerService = containerService;
+        }
+        public IActionResult Index()
+        {
+            return View(_containerService.GetAllContainerAndBlobs().GetAwaiter().GetResult());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AzureBlobProject/Services/Containerservice.cs
@@ -0,0 +1,68 @@
+﻿
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace AzureBlobProject.Services;
+
+public class Containerservice : IContainerService
+{
+    private readonly BlobServiceClient _blobClient;
+    public Containerservice(BlobServiceClient blobClient)
+    {
+        _blobClient = blobClient;
+    }
+    public async Task CreateContainer(string containerName)
+    {
+        BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+        await blobContainerClient.CreateIfNotExistsAsync(PublicAccessType.Blob);
+    }
+
+    public async Task DeleteContainer(string containerName)
+    {
+        BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+        await blobContainerClient.DeleteIfExistsAsync();
+    }
+
+    public async Task<List<string>> GetAllContainer()
+    {
+        List<string> containerName = new List<string>();
+        await foreach (var container in _blobClient.GetBlobContainersAsync())
+        {
+            containerName.Add(container.Name);
+        }
+        return containerName;
+    }
+
+    public async Task<List<string>> GetAllContainerAndBlobs()
+    {
+        List<string> containerAndBlobName = new();
+        containerAndBlobName.Add("-----Account Name : " + _blobClient.AccountName + "-----");
+        containerAndBlobName.Add("---------------------------------------------------------------");
+
+        await foreach (BlobContainerItem blobContainerItem in _blobClient.GetBlobContainersAsync())
+        {
+            containerAndBlobName.Add("-----" + blobContainerItem.Name);
+            BlobContainerClient _blobContainer = _blobClient.GetBlobContainerClient(blobContainerItem.Name);
+
+            await foreach (BlobItem blobItem in _blobContainer.GetBlobsAsync())
+            {
+                //get metadata
+
+                var blobClient = _blobContainer.GetBlobClient(blobItem.Name);
+                BlobProperties blobProperties = await blobClient.GetPropertiesAsync();
+                string tempBlobToAdd = blobItem.Name;
+                if (blobProperties.Metadata.ContainsKey("title"))
+                {
+                    tempBlobToAdd += "(" + blobProperties.Metadata["title"] + ")";
+                }
+
+                containerAndBlobName.Add(">>" + tempBlobToAdd);
+            }
+            containerAndBlobName.Add("---------------------------------------------------------------");
+        }
+
+        return containerAndBlobName;
+    }
+
+}
+
