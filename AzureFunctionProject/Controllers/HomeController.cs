cfb59a5b43b77577cd39389747d77e5131f122f3
Abhishek Kumar <akkr0077@gmail.com>
2025-12-14 23:44:36 +0530

    🚧WIP  Added initial code changes for Azure Function App

---
+++ AzureFunctionProject/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using AzureFunctionProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AzureFunctionProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
